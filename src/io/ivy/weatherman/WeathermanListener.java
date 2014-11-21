aa04c2181b1f96c3176405d1f24635eccfaf145c
Clint Moore <clint@ivy.io>
2014-11-21 05:42:34 -0800

    Florp

--- src/io/ivy/weatherman/WeathermanListener.java
+++ src/io/ivy/weatherman/WeathermanListener.java
@@ -1,26 +1,35 @@
 package io.ivy.weatherman;
 
+import java.util.Collection;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
 import net.canarymod.Canary;
 import net.canarymod.api.world.World;
 import net.canarymod.hook.HookHandler;
-import net.canarymod.hook.world.WeatherChangeHook;
+import net.canarymod.hook.system.ServerTickHook;
 import net.canarymod.plugin.PluginListener;
 
 public class WeathermanListener implements PluginListener {
+
 	@HookHandler
-	public void onWeatherChangeHook (WeatherChangeHook hook) {
-		Canary.log.info("*** WEATHER CHANGE");
-		Canary.log.info(hook.toString());
-		World world = hook.getWorld();
-		if (world.getThunderStrength() > 0) {
-			Canary.log.info("NO THUNDER PLEASE");
-			world.setThundering(false);
-			world.setRaining(false);
-		}
+	public void onServerTickHook(ServerTickHook hook) {
+		Collection<World> worlds = Canary.getServer().getWorldManager().getAllWorlds();
 		
-		if (world.getRainTicks() > 0) {
-			Canary.log.info("NO RAIN PLEASE");
-			world.setRaining(false);
-		}
+		Stream<World> raining_worlds = worlds.stream().filter(world -> world.isRaining());
+		Stream<World> thundering_worlds = worlds.stream().filter(world -> world.isThundering());
+		
+		thundering_worlds.forEach(new Consumer<World>() {
+			@Override
+			public void accept(World the_world) {
+				the_world.setThundering(false);
+			}
+		});
+		raining_worlds.forEach(new Consumer<World>() {
+			@Override
+			public void accept(World the_world) {
+				the_world.setRaining(false);
+			}
+		});
 	}
 }
